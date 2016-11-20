02abd7ad5934aa0de024417fecfc12665c564925
aditya7395 <aditya7395@gmail.com>
2016-11-20 14:12:07 -0800

    Updated the XML View

--- views/XMLPHPView.php
+++ views/XMLPHPView.php
@@ -1,36 +1,26 @@
 <?php
 namespace Hw4\CS174HW4\views;
 use Hw4\CS174HW4\views as V;
+
 class XMLPHPView
 {
 	function render($data)
 	{
-
 		renderXMLPHPView($xmlObj);
 	}
 
 	function renderXMLPHPView($xmlObj)
 	{
-		
 		$arr = (array) $xmlObj;
 		$keys = array_keys($arr);
-	 $values = array_values($arr);
+		$values = array_values($arr);
 
-	 for($i=0; $i<count($keys); $i++)
-	 {
-	 	
-	 	
-
-	 	print "<".$keys[$i].">".$values[$i]."<".$keys[$i].">";
-	 	echo "<br>";
-
-
-	 }
-
-	
-
+		for($i=0; $i<count($keys); $i++)
+		{
+			$test = "< $keys[$i]".">";
+			$test2 =  "< /$keys[$i]".">";
+			print $test.$values[$i].$test2;
+			echo "<br>";
+		}
 	}
-
-
-
 }
