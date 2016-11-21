4f8ae02fcf875a5893c5d0c727a2ecca07a373ab
aditya7395 <aditya7395@gmail.com>
2016-11-20 17:46:29 -0800

    Added form validations

--- index.php
+++ index.php
@@ -1,30 +1,27 @@
-
 <?php
-
 function format()
 {
-	return "here is the formatsdfadsf ";
+	return "here is the form";
 }
 
 $str =  format();
-
-echo "<!DOCTYPE html>
-<html>
-<head>
-	<title>PasteChart</title>
-	<link rel='stylesheet' type='text/css' href='Styles/styles.css'>
-</head>
-<body>
-<h1>PasteChart</h1>
-<p>Share your data in charts!</p>
-<form method='get' action='models/formAction.php'>
-<label> Chart Title</label>
-<input type='text' name='title' /><br>
-<textarea name='data'  rows='7' cols='50' placeholder=$str>
-</textarea><br>
-
-<input type='submit' name='sharebutton' value='Share' />
-</form>
-</body>
-</html>";
-
+?>
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>PasteChart</title>
+		<link rel='stylesheet' type='text/css' href='Styles/styles.css'>
+	</head>
+	<body>
+		<h1>PasteChart</h1>
+		<p>Share your data in charts!</p>
+		<form method='get' action='models/formAction.php'>
+			<label> Chart Title</label>
+			<input type='text' name='title' required/>
+			<br>
+			<textarea name='data' rows='7' cols='50' placeholder= "Data format should be a comma separated list of values, one per line, up to 50 lines of at most 80 characters, representing points to be plotted." required></textarea>
+			<br>
+			<input type='submit' name='sharebutton' value='Share'/>
+		</form>
+	</body>
+</html>
--- models/formAction.php
+++ models/formAction.php
@@ -1,85 +1,57 @@
 <?php
 namespace Hw4\CS174HW4\models;
 use Hw4\CS174HW4\controllers as C;
-$basePath = $_SERVER['DOCUMENT_ROOT'];
 
+$basePath = $_SERVER['DOCUMENT_ROOT'];
 $config2 = require("$basePath/Hw4/CS174HW4/configs/CreateDB.php");
 $config = require("$basePath/Hw4/CS174HW4/configs/config.php");
+
 $title = $_REQUEST['title'];
 echo "<br>";
-
-
-/*
 $datafromForm = $_REQUEST['data'];
-$index = strpos($datafromForm, "\n");
-
-
-for($i =0; i < strlen())
-//echo gettype($datafromForm);
-$data = substr($datafromForm, 0, $index-1);
-
-echo $data;
-*/
-
-$datafromForm = $_REQUEST['data'];
-
-
 $data = split("\n", $datafromForm);
-
 for($i =0; $i< count($data); $i++)
 {
 	echo $data[$i];
 	echo "<br>";
-
-
 }
 
 //hashing data and storing
 
 $hashValue = hash(md5, $datafromForm);
 echo gettype($hashValue);
-
 print $hashValue;
 PutinDatabase($config, $hashValue, $title, $datafromForm);
-
 require_once("./../controllers/lineGraphController.php");
     $main = new C\lineGraphController();
     $main->maincontrol();
 
-
 function PutinDatabase($config, $hash, $title, $datafromForm)
 {
-	$conn = mysqli_connect($config['host'], $config['username'], $config['password']);
-
-	 mysqli_select_db($conn,"Hw4DB");
-  
-
+  $conn = mysqli_connect($config['host'], $config['username'], $config['password']);
+  mysqli_select_db($conn,"Hw4DB");
   $sqlInsert = "INSERT INTO CHARTDATA (HASH, TITLE, DATA) VALUES ('$hash', '$title', '$datafromForm')";
 
-  if (mysqli_query($conn, $sqlInsert)) {
+  if (mysqli_query($conn, $sqlInsert)) 
+  {
     echo "New record created successfully";
-} else {
+  } 
+  else 
+  {
     echo "Error: " . $sqlInsert . "<br>" . mysqli_error($conn);
-}
-   
-    $result = mysqli_query($conn, "select * from CHARTDATA");
+  }
 
-    $num_rows = mysqli_num_rows($result);
-    echo "<br>";
-    print_r($num_rows);
-echo "<br>";
-$num_fields = mysqli_num_fields($result);
-echo $num_fields;
-for($j = 1; $j <= $num_rows; $j++)
-{
-	$row = mysqli_fetch_array($result);
-	print_r($row);
+  $result = mysqli_query($conn, "select * from CHARTDATA");
+  $num_rows = mysqli_num_rows($result);
   echo "<br>";
+  print_r($num_rows);
+  echo "<br>";
+  $num_fields = mysqli_num_fields($result);
+  echo $num_fields;
+  for($j = 1; $j <= $num_rows; $j++)
+  {
+	   $row = mysqli_fetch_array($result);
+	   print_r($row);
+      echo "<br>";
+  } 
 }
-
-    
-}
-
-
-
-
